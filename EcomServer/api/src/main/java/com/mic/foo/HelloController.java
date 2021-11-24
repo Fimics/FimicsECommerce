4e0e8437b50c01c67000c59f35a91a75b4da3bf4
li <lpjhblpj@163.com>
2021-11-24 19:09:59 +0800

    整合log4j打印日志

--- EcomServer/api/src/main/java/com/mic/foo/HelloController.java
+++ EcomServer/api/src/main/java/com/mic/foo/HelloController.java
@@ -1,5 +1,7 @@
 package com.mic.foo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +12,14 @@ import springfox.documentation.annotations.ApiIgnore;
 @ApiIgnore
 public class HelloController {
 
+    final static Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     @GetMapping("/hello")
     public String hello(){
+        logger.debug("debug : hello");
+        logger.error("error : hello");
+        logger.info("info : hello");
+        logger.warn("warn : hello");
         return "hello world";
     }
 }
