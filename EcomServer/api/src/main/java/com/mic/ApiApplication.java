a1e93f42b6e8df993e16b1fa6f3e703f298e38a7
li <lpjhblpj@163.com>
2021-11-25 16:06:27 +0800

    sql

--- EcomServer/api/src/main/java/com/mic/ApiApplication.java
+++ EcomServer/api/src/main/java/com/mic/ApiApplication.java
@@ -8,7 +8,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 @SpringBootApplication
 //扫描mybatis能用mapper所在的包
 @MapperScan(basePackages = "com.mic.mapper")
-// 扫描所有包以及相关组件包
+// idworker 扫描所有包以及相关组件包
 @ComponentScan(basePackages = {"com.mic", "org.n3r.idworker"})
 public class ApiApplication {
 
