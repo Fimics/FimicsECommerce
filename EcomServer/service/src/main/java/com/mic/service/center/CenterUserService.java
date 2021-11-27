b1d6f8b8c9f8983d9193fe0ca081e5251661b918
li <lpjhblpj@163.com>
2021-11-28 00:40:19 +0800

    用户中心 - 查询用户信息

---
+++ EcomServer/service/src/main/java/com/mic/service/center/CenterUserService.java
@@ -0,0 +1,30 @@
+package com.mic.service.center;
+
+
+import com.mic.bo.center.CenterUserBO;
+import com.mic.pojo.Users;
+
+public interface CenterUserService {
+
+    /**
+     * 根据用户id查询用户信息
+     * @param userId
+     * @return
+     */
+    public Users queryUserInfo(String userId);
+
+    /**
+     * 修改用户信息
+     * @param userId
+     * @param centerUserBO
+     */
+    public Users updateUserInfo(String userId, CenterUserBO centerUserBO);
+
+    /**
+     * 用户头像更新
+     * @param userId
+     * @param faceUrl
+     * @return
+     */
+    public Users updateUserFace(String userId, String faceUrl);
+}
---
+++ EcomServer/service/src/main/java/com/mic/service/center/impl/MyCommentsServiceImpl.java
@@ -0,0 +1,95 @@
+package com.mic.service.center.impl;
+
+import com.github.pagehelper.PageHelper;
+
+import com.mic.bo.center.OrderItemsCommentBO;
+import com.mic.enums.YesOrNo;
+import com.mic.mapper.ItemsCommentsMapperCustom;
+import com.mic.mapper.OrderItemsMapper;
+import com.mic.mapper.OrderStatusMapper;
+import com.mic.mapper.OrdersMapper;
+import com.mic.pojo.OrderItems;
+import com.mic.pojo.OrderStatus;
+import com.mic.pojo.Orders;
+import com.mic.service.center.MyCommentsService;
+import com.mic.utils.PagedGridResult;
+import com.mic.vo.MyCommentVO;
+import org.n3r.idworker.Sid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class MyCommentsServiceImpl extends BaseService implements MyCommentsService {
+
+    @Autowired
+    public OrderItemsMapper orderItemsMapper;
+
+    @Autowired
+    public OrdersMapper ordersMapper;
+
+    @Autowired
+    public OrderStatusMapper orderStatusMapper;
+
+    @Autowired
+    public ItemsCommentsMapperCustom itemsCommentsMapperCustom;
+
+    @Autowired
+    private Sid sid;
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public List<OrderItems> queryPendingComment(String orderId) {
+        OrderItems query = new OrderItems();
+        query.setOrderId(orderId);
+        return orderItemsMapper.select(query);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public void saveComments(String orderId, String userId,
+                             List<OrderItemsCommentBO> commentList) {
+
+        // 1. 保存评价 items_comments
+        for (OrderItemsCommentBO oic : commentList) {
+            oic.setCommentId(sid.nextShort());
+        }
+        Map<String, Object> map = new HashMap<>();
+        map.put("userId", userId);
+        map.put("commentList", commentList);
+        itemsCommentsMapperCustom.saveComments(map);
+
+        // 2. 修改订单表改已评价 orders
+        Orders order = new Orders();
+        order.setId(orderId);
+        order.setIsComment(YesOrNo.YES.type);
+        ordersMapper.updateByPrimaryKeySelective(order);
+
+        // 3. 修改订单状态表的留言时间 order_status
+        OrderStatus orderStatus = new OrderStatus();
+        orderStatus.setOrderId(orderId);
+        orderStatus.setCommentTime(new Date());
+        orderStatusMapper.updateByPrimaryKeySelective(orderStatus);
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public PagedGridResult queryMyComments(String userId,
+                                           Integer page,
+                                           Integer pageSize) {
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("userId", userId);
+
+        PageHelper.startPage(page, pageSize);
+        List<MyCommentVO> list = itemsCommentsMapperCustom.queryMyComments(map);
+
+        return setterPagedGrid(list, page);
+    }
+}
